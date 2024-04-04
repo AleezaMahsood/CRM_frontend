f8f74269df3b5317b4262ccd532d0d863e84474e
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-04-03 18:06:50 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Login from "@/views/Login";
 import CreateLeads from "@/views/CreateLeads";
+import CreateProject from "@/views/CreateProjects";
+import CreateCampaign from "@/views/CreateCampaigns";
 import Navbar from "@/User/views/navbar"
 import Createform from "@/User/views/Createform";
 import CreateUsers from "@/views/CreateUsers";
