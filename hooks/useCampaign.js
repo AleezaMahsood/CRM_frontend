733613142f8c9ae354a45e8855390ac3e17eadd1
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-04-03 18:01:18 -0700

    Create useCampaign.js

---
+++ hooks/useCampaign.js
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const useCampaign = () => {
+  return useQuery({
+    queryKey: ["Campaignsdata"],
+    queryFn: () =>
+      axios.get("http://127.0.0.1:8000/api/Campaigns").then((res) => res.data),
+  });
+};
+export default useCampaign;
