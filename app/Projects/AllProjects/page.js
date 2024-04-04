d53a5e3b1d37791bd5142ce14890649641f724ab
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-04-03 18:05:09 -0700

    Add files via upload

---
+++ app/Projects/AllProjects/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>All Projects</div>;
+};
+
+export default page;
---
+++ app/Projects/CreateProjects/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+import CreateProject from "@/views/CreateProjects";
+const page = () => {
+  return <CreateProject />;
+};
+
+export default page;
