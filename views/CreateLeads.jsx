f7ea05ffcbc98b1460649fc9cc09fa6098524576
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-05-14 09:47:33 -0700

    Update CreateLeads.jsx

--- views/CreateLeads.jsx
+++ views/CreateLeads.jsx
@@ -1,87 +1,195 @@
 "use client";
 import React from "react";
-import useLeads from "@/hooks/useLeads";
-import axios from "@/utils/axios"
+import axios from "@/utils/axios";
+import { useLeadEnums } from '@/hooks/useLeadEnums';
+
 const CreateLeads = () => {
-  //const {data,loading,error}=useLeads();
-  const submitForm=(e)=>{
+  const submitForm = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    //formData.append('_token', document.querySelector('meta[name="csrf-token"]').content);
     const frmData = {
-      leadName: formData.get('leadName'), // Name of the lead
-      phoneNumber: formData.get('phoneNumber'), // Phone number of the lead
-      project: formData.get('project'), // Project associated with the lead
-      campaign: formData.get('campaign'), // Campaign associated with the lead
-      lead_cost: formData.get('lead_cost'), // Cost associated with the lead
-      lead_date: formData.get('lead_date'), // Date associated with the lead
-    //  categories: [formData.get('category')] // Category ID
-  };
-  
-    
-  console.log(frmData);
-  axios.post(
-    "/api/leads",
-    frmData,
-    {
+      leadName: formData.get("leadName"),
+      phoneNumber: formData.get("phoneNumber"),
+      project: formData.get("project"),
+      campaign: formData.get("campaign"),
+      budget: formData.get("budget"),
+      lead_date: formData.get("date"),
+      status: formData.get('status'), // corrected to match the input's name
+    };
+
+    console.log(frmData);
+    axios.post("/api/leads", frmData, {
       headers: {
-        "Content-Type": "application/json"
-      }
-    }
-  );
-   }
-  
+        "Content-Type": "application/json",
+      },
+    });
+  };
+  const { data: enumsData } = useLeadEnums();
+
   return (
     <>
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 px-4 mx-auto max-w-xl lg:max-w-2xl">
-            <h2 className="mb-4 text-2xl font-bold text-gray-900 dark:text-white">New Lead</h2>
-            <hr className="mb-4" />
-            <form method="post" action="#" onSubmit={submitForm} className="space-y-6">
+          <h2 className="mb-4 text-2xl font-bold text-gray-900 dark:text-white">
+            New Lead
+          </h2>
+          <hr className="mb-4" />
+          <form
+            method="post"
+            action="#"
+            onSubmit={submitForm}
+            className="space-y-6"
+          >
+            <div>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">
+                Lead Details
+              </h3>
+              <p className="text-gray-500 text-sm mb-4">
+                Add details such as name, phone number, date, and project.
+              </p>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
                 <div>
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">Lead Details</h3>
-                    <p className="text-gray-500 text-sm mb-4">Add details such as name, phone number, date and project.</p>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                        <div>
-                            <label for="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name:</label>
-                            <input type="text" name="leadName" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter campaign name" required="" />
-                        </div>
-                        <div>
-                            <label for="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date:</label>
-                            <input type="text" name="lead_date" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter campaign description" required="" />
-                        </div>
-                    </div>
+                  <label
+                    htmlFor="name"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Name:
+                  </label>
+                  <input
+                    type="text"
+                    name="leadName"
+                    id="name"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Enter lead name"
+                    required
+                  />
                 </div>
                 <div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                        <div>
-                            <label for="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone:</label>
-                            <input type="text" name="phoneNumber" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter campaign name" required="" />
-                        </div>
-                        <div>
-                            <label for="project" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Project:</label>
-                            <input type="text" name="project" id="project" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Enter campaign description" required="" />
-                        </div>
-                    </div>
-                </div>              
-                <div>
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">Lead Campaign</h3>
-                    <p className="text-gray-500 text-sm mb-4">Select campaign for the lead. This section is optional.</p>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-7">
-                        <div>
-                            <label for="campaign" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Campaign:</label>
-                            <input type="text" name="campaign" id="revenue" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="e.g:100,000,000" required="" />
-                        </div>
-                        <div>
-                            <label for="cost" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Lead Cost:</label>
-                            <input type="cost" name="lead_cost" id="cost" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="e.g:10,000,000" required="" />
-                        </div>
-                    </div>
+                  <label
+                    htmlFor="date"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Date:
+                  </label>
+                  <input
+                    style={{ width: "100%" }}
+                    type="date"
+                    name="date"
+                    id="date"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="-----"
+                    required
+                  />
                 </div>
-                <input type="submit" value="Add Campaign" className="inline-flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" />
-            </form>
+              </div>
+            </div>
+            <div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                <div>
+                  <label
+                    htmlFor="phone"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Phone:
+                  </label>
+                  <input
+                    type="text"
+                    name="phoneNumber"
+                    id="phone"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Enter phone number"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="project"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Project:
+                  </label>
+                  <input
+                    type="text"
+                    name="project"
+                    id="project"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="Enter project description"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white mb-2">
+                Lead Campaign
+              </h3>
+              <p className="text-gray-500 text-sm mb-4">
+                Select campaign for the lead. This section is optional.
+              </p>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-7">
+                <div>
+                  <label
+                    htmlFor="campaign"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Campaign:
+                  </label>
+                  <input
+                    type="text"
+                    name="campaign"
+                    id="campaign"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="e.g. 100,000,000"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="budget"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Budget:
+                  </label>
+                  <input
+                    type="text"
+                    name="budget"
+                    id="budget"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    placeholder="e.g. 10,000,000"
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="status"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Status:
+                </label>
+                <select
+                  name="status"
+                  id="status"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  required
+                >
+                  <option value="" disabled selected>Select Status</option>
+                  {enumsData?.statuses?.map(status => (
+                    <option key={status} value={status}>{status}</option> 
+                  ))}
+                </select>
+              </div>
+            </div>
+            <input
+              type="submit"
+              value="Add Lead"
+              className="inline-flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            />
+          </form>
         </div>
-    </section>
+      </section>
     </>
   );
 };
