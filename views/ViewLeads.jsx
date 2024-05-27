f893a6b9e61a24c18c6c66304e5e93f0e9821047
Fatima Nadeem <156994758+FatimaNW@users.noreply.github.com>
2024-05-28 02:00:32 +0500

    Update ViewLeads.jsx

--- views/ViewLeads.jsx
+++ views/ViewLeads.jsx
@@ -3,24 +3,30 @@ import React from 'react'
 import Table from '@/components/Table';
 import useLeads from '@/hooks/useLeads';
 const ViewLeads = () => {
-   const{data,isLoading,isError}=useLeads()
+  const{data,isLoading,isError}=useLeads()
+
+  /*const data = [
+    { id: 1, leadName: 'Fatima', phone: '0333 1234567', project: 'Project Alpha', date: '2024-05-01', user_id: 'Ahmed', Dept: 'Sales', status: 'Active' },
+    { id: 2, leadName: 'Fatima 2', phone: '0677 9876543', project: 'Project Beta', date: '2024-05-27', user_id: 'Amna', Dept: 'CS', status: 'Active' },
+  ];*/
     
-      // Define headings and rows
-      const headings = ['#', 'CLIENT', 'PROJECT', 'LEAD DATE', 'ASSIGNED TO', 'STATUS'];
-      const rows = ['id', 'leadName', 'project', 'date', 'phone', 'status'];
-    return (
-        <div>
-          <Table
-            data={data}
-            tableId="exampleTable"
-            entriesPerPageSelectId="entriesPerPageSelect"
-            paginationId="pagination"
-            headings={headings}
-            rows={rows}
-          />
-        </div>
-      );
-  
+  // Define headings and rows
+  const headings = ['#', 'CLIENT', 'PROJECT', 'LEAD DATE', 'ASSIGNED TO', 'STATUS'];
+  const rows = ['id', 'leadNamePhone', 'project', 'date', 'user_id_Dept', 'status'];
+
+  return (
+    <div>
+      <Table
+        title="All Leads"
+        data={data}
+        tableId="exampleTable"
+        entriesPerPageSelectId="entriesPerPageSelect"
+        paginationId="pagination"
+        headings={headings}
+        rows={rows}
+      />
+    </div>
+  );
 }
 
-export default ViewLeads
+export default ViewLeads;
