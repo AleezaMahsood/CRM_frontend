122a5e2981f2e16d6077e171eef296af14e58284
Arfa Ahsan <150623089+Arfa-Ahsan@users.noreply.github.com>
2024-06-09 14:45:22 -0700

    Update ViewUserLeads.jsx

--- User/view/ViewUserLeads.jsx
+++ User/view/ViewUserLeads.jsx
@@ -1,117 +1,121 @@
 'use client';
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import EditLeadForm from './EditLeads';
 import LeadTable from '@/components/LeadTable';
 import useSpecificLead from '@/hooks/useSpecificLead';
+import useUpdateLead from '@/hooks/useUpdateLeads';
+import useProjects from '@/hooks/useProject';
+import useUserLeads from '@/hooks/useUserLeads';
+
+const fetchUserData = async () => {
+  const response = await fetch("http://localhost:8000/api/auth/me", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch user data");
+  }
+  return response.json();
+};
 
 const ViewUserLeads = () => {
-  const [token, setToken] = useState("");
   const [userId, setUserId] = useState("");
-  const [leads, setLeads] = useState([]);
-  const [projects, setProjects] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const [filters, setFilters] = useState({});
+  const [selectedFilter, setSelectedFilter] = useState('');
   const [editLeadId, setEditLeadId] = useState(null);
-  const { data: editLead, isLoading: isEditLoading } = useSpecificLead(editLeadId);
 
   useEffect(() => {
-    const fetchUserData = async () => {
+    const fetchUser = async () => {
       try {
-        const response = await fetch("http://localhost:8000/api/auth/me", {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-            "Content-Type": "application/json",
-          },
-        });
-        if (!response.ok) {
-          throw new Error("Failed to fetch user data");
-        }
-        const userData = await response.json();
+        const userData = await fetchUserData();
         setUserId(userData.id);
-        await fetchUserLeads(userData.id);
-        await fetchProjects();
       } catch (error) {
         console.error("Error fetching user data:", error);
-        setIsError(true);
-        setIsLoading(false);
       }
     };
-
-    const fetchUserLeads = async (userId) => {
-      try {
-        const response = await axios.get(`http://localhost:8000/api/user/${userId}/lead`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
-        setLeads(response.data);
-      } catch (error) {
-        console.error("Error fetching user leads:", error);
-        setIsError(true);
-        setIsLoading(false);
-      }
-    };
-
-    const fetchProjects = async () => {
-      try {
-        const response = await axios.get("http://localhost:8000/api/projects", {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
-        setProjects(response.data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching projects:", error);
-        setIsError(true);
-        setIsLoading(false);
-      }
-    };
-
-    fetchUserData();
+    fetchUser();
   }, []);
 
-  const handleEdit = (leadId) => {
-    setEditLeadId(leadId);
-  };
+  const { data: leads, isLoading: isLeadsLoading, isError: isLeadsError } = useUserLeads(userId);
+  const { data: projects, isLoading: isProjectsLoading, isError: isProjectsError } = useProjects();
+  const { data: editLead, isLoading: isEditLoading } = useSpecificLead(editLeadId);
+  const mutation = useUpdateLead();
 
   const handleUpdate = async (updatedLead) => {
     try {
-      await axios.put(`http://localhost:8000/api/leads/${updatedLead.id}`, updatedLead, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-      const updatedLeads = leads.map(lead => lead.id === updatedLead.id ? updatedLead : lead);
-      setLeads(updatedLeads);
+      await mutation.mutateAsync({ id: updatedLead.id, formData: updatedLead });
       setEditLeadId(null);
     } catch (error) {
       console.error('Error updating lead:', error);
     }
   };
 
-  const getProjectName = (projectId) => {
-    const project = projects.find(project => project.id === projectId);
-    return project ? project.project_name : "Unknow Project";
+  const handleEdit = (leadId) => {
+    setEditLeadId(leadId);
   };
 
-  const enhancedLeads = leads.map(lead => ({
-    ...lead,
-    project_name: getProjectName(lead.project_id)
-  }));
+  const getProjectName = (projectId) => {
+    const project = projects?.find((project) => project.id === projectId);
+    return project ? project.project_name : "Unknown Project";
+  };
+
+  const enhancedLeads = useMemo(() => {
+    return leads?.map((lead) => ({
+      ...lead,
+      project_name: getProjectName(lead.project_id),
+    })) || [];
+  }, [leads, projects]);
+
+  const applyFilters = () => {
+    if (!enhancedLeads) return [];
+    return enhancedLeads.filter((lead) => {
+      return Object.keys(filters).every((key) => {
+        const value = lead[key]?.toString().toLowerCase();
+        return value && value.includes(filters[key].toLowerCase());
+      });
+    });
+  };
+
+  const filteredData = useMemo(() => applyFilters(), [filters, enhancedLeads]);
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
+  };
+
+  const handleSelectedFilterChange = (e) => {
+    setSelectedFilter(e.target.value);
+  };
+
+  const clearFilters = () => {
+    setFilters({});
+    setSelectedFilter('');
+  };
+
+  const filterOptions = [
+    { value: 'leadName', label: 'Client' },
+    { value: 'project_name', label: 'Project' },
+    { value: 'date', label: 'Lead Date' },
+    { value: 'status', label: 'Status' },
+  ];
 
   const headings = ['#', 'CLIENT', 'PROJECT', 'LEAD DATE', 'STATUS'];
   const rows = ['id', 'leadNamePhone', 'project_name', 'date', 'status'];
 
-  if (isLoading || isEditLoading) {
+  if (isLeadsLoading || isProjectsLoading || isEditLoading) {
     return <div>Loading...</div>;
   }
 
-  if (isError) {
-    return <div>Error fetching leads.</div>;
+  if (isLeadsError || isProjectsError) {
+    return <div>Error fetching data.</div>;
   }
 
   return (
@@ -127,9 +131,56 @@ const ViewUserLeads = () => {
         </div>
       ) : (
         <div>
+          <div style={{ marginTop: '20px', marginRight: '20px' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'right' }}>
+              <div></div>
+              <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
+                <select
+                  value={selectedFilter}
+                  onChange={handleSelectedFilterChange}
+                  style={{ marginRight: '10px', padding: '8px', borderRadius: '4px', border: '1px solid gray', width: '150px' }}
+                >
+                  <option value="">Filter Records</option>
+                  {filterOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+                {selectedFilter && (
+                  <input
+                    type={selectedFilter === 'date' ? 'date' : 'text'}
+                    name={selectedFilter}
+                    placeholder={`Enter ${filterOptions.find((opt) => opt.value === selectedFilter)?.label}`}
+                    value={filters[selectedFilter] || ''}
+                    onChange={handleFilterChange}
+                    style={{ padding: '8px', borderRadius: '4px', border: '1px solid gray', width: '150px' }}
+                  />
+                )}
+                <button
+                  onClick={clearFilters}
+                  style={{ marginLeft: '10px', padding: '8px', borderRadius: '4px', border: '1px solid gray' }}
+                >
+                  Clear Filters
+                </button>
+              </div>
+            </div>
+            <div style={{ marginTop: '10px', display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-end' }}>
+              {Object.keys(filters).map((key) => (
+                filters[key] && (
+                  <div
+                    key={key}
+                    style={{ margin: '5px', padding: '5px 10px', borderRadius: '5px', background: '#B0E0FC' }}
+                  >
+                    {filterOptions.find((opt) => opt.value === key)?.label}: {filters[key]}
+                  </div>
+                )
+              ))}
+            </div>
+          </div>
           <LeadTable
             title="All Leads"
-            data={Array.isArray(enhancedLeads) ? enhancedLeads : []}
+            data={filteredData}
             tableId="exampleTable"
             entriesPerPageSelectId="entriesPerPageSelect"
             paginationId="pagination"
@@ -140,7 +191,7 @@ const ViewUserLeads = () => {
         </div>
       )}
     </div>
-  ); 
+  );
 };
 
 export default ViewUserLeads;
